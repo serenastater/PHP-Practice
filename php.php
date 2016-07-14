6b4f5f9157249853d9a9529747aee107bc04999d
Serena Stater <srstater@gmail.com>
2016-07-13 21:56:51 -0400

    do/while loop

--- php.php
+++ php.php
@@ -13,5 +13,19 @@
 			$headCount = 0;
 		}
 	}
-	echo "It took {$flipCount} flips!";
+	print "It took {$flipCount} flips!";
+
+  // Flip coin as long as result is heads
+  $flipCount = 0;
+	do {
+		$flip = rand(0,1);
+		$flipCount ++;
+	} while ($flip);
+	$verb = "were";
+	$last = "flips";
+	if ($flipCount == 1) {
+		$verb = "was";
+		$last = "flip";
+	}
+	print "There {$verb} {$flipCount} {$last}!";
 ?>
